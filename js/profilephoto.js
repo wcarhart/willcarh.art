ad55eeacd1f54bccf1f06ff01af956cb6b926a5f
Will Carhart <carhartwill@gmail.com>
2020-11-03 10:53:36 -0800

    Working on about

--- js/profilephoto.js
+++ js/profilephoto.js
@@ -12,14 +12,14 @@ $(document).ready(async () => {
 		'https://storage.googleapis.com/willcarh-art/img/profile2.jpeg'
 	]
 
-	let index = 0
+	let index = 1
 	do {
+		await new Promise(res => setTimeout(res, 7*1000));
 		let image = $('#profile-img')
 		image.fadeOut('slow', async () => {
 	        image.attr('src', photos[index])
 	        image.fadeIn('slow')
 	    })
-		await new Promise(res => setTimeout(res, 7*1000));
 		index += 1
 		index = index % photos.length
 	} while (true)

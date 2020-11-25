73790a12b197b455073cb1d01684960b5696593e
Will Carhart <carhartwill@gmail.com>
2020-11-25 12:21:45 -0700

    Working on dynamic sizing updates

--- content/projects.md
+++ content/projects.md
@@ -2,6 +2,7 @@ name: lurker
 blurb: Hacker News terminal client
 about: Lurker is a simple terminal client for reading Y Combinator's Hacker News. It uses the public Hacker News API and is written entirely in Bash so it runs smoothly in the majority of terminal environments.
 about: Lurker embraces the hacker in Hacker News by exposing a simple CLI for reading stories, comments, and user information. This makes it easy to read Hacker News while completing other command line activities, especially when combined with tools like tmux.
+about: In addition to browsing Hacker News, lurker can summarize most text articles and snippets via another one of my tools, smoosh.
 languages: Bash
 technologies: Hacker News API
 img: {{cdn:img/logos/lurker.png}}
--- js/projectcards.js
+++ js/projectcards.js
@@ -2,6 +2,14 @@
 
 // handle hover and click for project cards
 $(document).ready(async () => {
+
+	// TODO: why does this fail when doing hard refresh
+	await resizeProjectContainers()
+	// TODO: fix this garbage, doesn't call function when window is resized
+	$(window).resize(async () => { 
+		await resizeProjectContainers()
+	})
+
 	// handle animations for hover
 	$('.project-border').hover(async function() {
 		const id = `#title-${this.id.replace('proj-', '')}`
@@ -18,3 +26,30 @@ $(document).ready(async () => {
 		console.log(this.id)
 	})
 })
+
+const resizeProjectContainers = async () => {
+	// calculate dynamic row heights
+	let miniTopHeight = 0
+	let miniBottomHeight = 0
+	for (let el of $('.proj-mini-top')) {
+		if ($(el).height() > miniTopHeight) {
+			miniTopHeight = $(el).height()
+		}
+	}
+	for (let el of $('.proj-mini-bottom')) {
+		if ($(el).height() > miniBottomHeight) {
+			miniBottomHeight = $(el).height()
+		}
+	}
+
+	// update row heights
+	for (let el of $('.proj-mini-top')) {
+		$(el).height(miniTopHeight)
+	}
+	for (let el of $('.proj-mini-bottom')) {
+		$(el).height(miniBottomHeight)
+	}
+	// spacer small is 2rem, and 1rem = 16px - need to add 4.5rem (72px) for spacing
+	$('.proj-main').height(miniTopHeight + 72 + miniBottomHeight)
+	$('.proj-main').css('padding-top', '2rem')
+}

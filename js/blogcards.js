a0c3029ec4050db60d687ea64ef14aec23164106
Will Carhart <carhartwill@gmail.com>
2020-11-27 20:08:54 -0700

    Added prototype content for blog index

---
+++ js/blogcards.js
@@ -0,0 +1,5 @@
+{{sys:headerjs}}
+
+// TODO: add underline to titles on blog cards
+// TODO: handle clicks on individual blog cards
+// TODO: make blog cards in each row equal height (should be much easier than project cards)
--- src/js/blogcards.js
+++ src/js/blogcards.js
@@ -2,8 +2,6 @@
 // This file was generated on Fri Nov 27 2020 20:08:06 GMT-0700 (Mountain Standard Time) via the forge in willcarh.art v2.0.0
 // Learn more: https://github.com/wcarhart/willcarh.art
 
-// dynamically update footer year
-const year = new Date().getFullYear();
-$(document).ready(async () => {
-	$('#year').text(year)
-})
+// TODO: add underline to titles on blog cards
+// TODO: handle clicks on individual blog cards
+// TODO: make blog cards in each row equal height (should be much easier than project cards)

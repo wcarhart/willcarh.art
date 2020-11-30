284aaa9c0b82f3b67bd16cbde5b885da4dec33a9
Will Carhart <carhartwill@gmail.com>
2020-11-30 14:31:06 -0800

    Finalized etc

--- js/etc.js
+++ js/etc.js
@@ -3,14 +3,24 @@
 // dynamically update items on etc page
 
 $(document).ready(async () => {
-	$('#color-palette-color-background').text(rgb2hex($('#color-palette-color-background').css('color')))
-	$('#color-palette-color-detail').text(rgb2hex($('#color-palette-color-detail').css('color')))
-	$('#color-palette-color-subtitle').text(rgb2hex($('#color-palette-color-subtitle').css('color')))
-	$('#color-palette-color-color').text(rgb2hex($('#color-palette-color-color').css('color')))
-	$('#color-palette-color-text').text(rgb2hex($('#color-palette-color-text').css('color')))
+	// dynamically add color hex values based on page color
+	await updateColorSwatch()
+	$('#dark-mode-toggle').click(async () => {
+		await updateColorSwatch()
+	})
 })
 
-function rgb2hex(rgb) {
+// update the color swatch items
+const updateColorSwatch = async () => {
+	$('#color-palette-color-background').text(await rgb2hex($('#color-palette-color-background').css('color')))
+	$('#color-palette-color-detail').text(await rgb2hex($('#color-palette-color-detail').css('color')))
+	$('#color-palette-color-subtitle').text(await rgb2hex($('#color-palette-color-subtitle').css('color')))
+	$('#color-palette-color-color').text(await rgb2hex($('#color-palette-color-color').css('color')))
+	$('#color-palette-color-text').text(await rgb2hex($('#color-palette-color-text').css('color')))
+}
+
+// convert RGB to hex color values
+const rgb2hex = async (rgb) => {
 	rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
 	function hex(x) {
 		return ("0" + parseInt(x).toString(16)).slice(-2);
